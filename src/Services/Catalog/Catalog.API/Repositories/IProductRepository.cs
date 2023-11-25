dd13ae09e514805b553cd02d6042cce6c078d421
Fatma <fmutlu3002@gmail.com>
2023-11-25 18:46:31 +0300

    Repository pattern was implemented.

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetProductAsync(string id);
+        Task<IEnumerable<Product>> GetProductsByNameAsync(string name);
+        Task<IEnumerable<Product>> GetProductsByCategory(string categoryName);
+        Task AddAsync(Product product); 
+        Task<bool> UpdateAsync(Product product);
+        Task<bool> DeleteAsync(string id);
+    }
+}
