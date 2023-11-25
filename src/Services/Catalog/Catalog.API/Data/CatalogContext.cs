b54d3ef7e1fa91e22b6d51dc463af37b2a41e5c3
Fatma <fmutlu3002@gmail.com>
2023-11-25 14:22:53 +0300

    created mongodb connection and applied seed operations

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext :ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("MongoSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("MongoSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("MongoSettings:CollectionName"));
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
