aabf1a487949e94a74f90a524ca95ce8833df30a
Fatma <fmutlu3002@gmail.com>
2023-11-28 11:45:21 +0300

    Discount controller and Automapper

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,6 @@
 using Discount.Grpc.Extensions;
 using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,15 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddGrpc();
-
 builder.Services.AddScoped<IDiscountRepository,DiscountRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.MigrateDatabase<Program>();
