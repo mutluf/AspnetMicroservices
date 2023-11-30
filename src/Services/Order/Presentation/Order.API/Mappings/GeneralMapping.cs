d31b0f475962cfdc41bb4a3c21bdfa19ae71baec
Fatma <fmutlu3002@gmail.com>
2023-11-30 19:20:26 +0300

    docker compose

---
+++ src/Services/Order/Presentation/Order.API/Mappings/GeneralMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using Order.Application.Features.Orders.Commands.CheckoutOrder;
+
+namespace Order.API.Mappings
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<CheckoutOrderCommandRequest,BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
--- src/Services/Order/Presentation/Order.API/Program.cs
+++ src/Services/Order/Presentation/Order.API/Program.cs
@@ -3,6 +3,9 @@ using Order.Persistence;
 using Order.Infrastructure;
 using Order.Persistence.Context;
 using Order.API.Extensions;
+using MassTransit;
+using EventBus.Messages.Common;
+using Order.API.Consumers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,13 +16,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//builder.Services.MigrateDatabase<OrderDbContext>((context, services) =>
-//{
-//    var logger = services.GetService<ILogger<OrderContextDbSeed>>();
-//    OrderContextDbSeed
-//        .SeedAsync(context, logger)
-//        .Wait();
-//});
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<BasketCheckoutConsumer>();
+
+
+    config.UsingRabbitMq((context, configurator) =>
+    {
+        configurator.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+
+
+        configurator.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c => c.ConfigureConsumer<BasketCheckoutConsumer>(context));
+
+    });
+});
+
+//builder.Services.AddScoped<BasketCheckoutConsumer>();
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
