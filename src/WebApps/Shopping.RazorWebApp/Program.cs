6d22fbbde739f7fbc25ea5670e2225c437ad2c04
Fatma <fmutlu3002@gmail.com>
2023-12-02 19:24:01 +0300

    web app models

--- src/WebApps/Shopping.RazorWebApp/Program.cs
+++ src/WebApps/Shopping.RazorWebApp/Program.cs
@@ -1,20 +1,8 @@
-using Shopping.RazorWebApp.Data;
-using Shopping.RazorWebApp.Repositories.Abstractions;
-using Shopping.RazorWebApp.Repositories;
-using Microsoft.EntityFrameworkCore;
-using Shopping.RazorWebApp.Extensions;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<ShoppingDbContext>(c =>
-                c.UseSqlServer(builder.Configuration.GetConnectionString("ShoppingConnection")));
 
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<ICartRepository, CartRepository>();
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
-builder.Services.AddScoped<IContactRepository, ContactRepository>();
 
 var app = builder.Build();
 
@@ -30,5 +18,5 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-app.SeedDatabase();
+
 app.Run();
